67042e6c6dad0714a17e40d120e3139e47f1461c
jleeson <jleeson2000@gmail.com>
2021-03-19 11:34:57 -0700

    added publicPath toolchain option

--- packages/toolchain/src/configs/default.js
+++ packages/toolchain/src/configs/default.js
@@ -5,6 +5,7 @@ export const defaultOptions = {
     minify: (process.env.NODE_ENV == "production") ? true : false,
     sourcemap: false,
     paths: {},
+    publicPath: "/",
     devServer: {
         open: (process.env.NODE_ENV == "development") ? true : false,
         port: 3000,
--- packages/toolchain/src/configs/rollup.js
+++ packages/toolchain/src/configs/rollup.js
@@ -124,7 +124,7 @@ export default function getRollupConfig({ config, options }) {
         plugins.push(
             html({
                 title: config.name,
-                publicPath: "/",
+                publicPath: options.publicPath,
                 template: renderHTML
             })
         );
