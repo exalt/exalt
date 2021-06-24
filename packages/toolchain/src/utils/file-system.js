29e75a9199a92095ea9cc5b8218a611d7fd29f22
jleeson <jleeson2000@gmail.com>
2021-06-24 14:53:23 -0700

    fix: assets not being moved properly

--- packages/toolchain/src/utils/file-system.js
+++ packages/toolchain/src/utils/file-system.js
@@ -6,15 +6,14 @@ export function copyFolder(src, dest, copyOnce = false) {
     const filesToCreate = fs.readdirSync(src);
 
     for (let file of filesToCreate) {
-        if (file.endsWith(".html")) continue;
+        if (file == "index.html") continue;
 
         const originalPath = path.join(src, file);
         const newPath = path.join(dest, file);
         const stats = fs.statSync(originalPath);
 
-        if (copyOnce && fs.existsSync(newPath)) continue;
-
         if (stats.isFile()) {
+            if (copyOnce && fs.existsSync(newPath)) continue;
             fs.writeFileSync(newPath, fs.readFileSync(originalPath));
         } else if (stats.isDirectory()) {
             if (!fs.existsSync(newPath)) {
