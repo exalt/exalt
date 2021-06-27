3bf9060c2fe7aa70e14ec9402189b1a5f364fed6
jleeson <jleeson2000@gmail.com>
2021-06-26 17:47:17 -0700

    fix: copyFolder function improvements

--- packages/toolchain/src/utils/file-system.js
+++ packages/toolchain/src/utils/file-system.js
@@ -1,20 +1,19 @@
 import fs from "fs";
 import path from "path";
 
-/* copy folder to another folder */
+/* copy a folder to another folder */
 export function copyFolder(src, dest, copyOnce = false) {
     const filesToCreate = fs.readdirSync(src);
 
     for (let file of filesToCreate) {
-        if (file.endsWith(".html")) continue;
-
         const originalPath = path.join(src, file);
         const newPath = path.join(dest, file);
         const stats = fs.statSync(originalPath);
 
-        if (copyOnce && fs.existsSync(newPath)) continue;
+        if (originalPath == "public/index.html") continue;
 
         if (stats.isFile()) {
+            if (copyOnce && fs.existsSync(newPath)) continue;
             fs.writeFileSync(newPath, fs.readFileSync(originalPath));
         } else if (stats.isDirectory()) {
             if (!fs.existsSync(newPath)) {
